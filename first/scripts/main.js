9c0b372ad2d16ba914f33ae38bf921ac82bde60e
g19922ma <52483478+g19922ma@users.noreply.github.com>
2020-12-11 00:42:34 +0900

    Update main.js

--- first/scripts/main.js
+++ first/scripts/main.js
@@ -1,6 +1,3 @@
-      let myButton = document.querySelector('button');
-      let myHeading = document.querySelector('h1');
-     
       function setUserName() {
       let myName = prompt('あなたの名前を入力してください。');
       localStorage.setItem('name', myName);
